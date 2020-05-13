92d4a6c035f6b640f78c2cf3a12a49a7993b2668
Fatma Akram <fatma.khater96@gmail.com>
2020-05-13 20:04:50 +0200

    Initialize route files

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
 connectDB();
+
 app.get('/', (req, res) => {
     res.send("Hello");
   });
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/messages', require('./routes/api/messages'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
