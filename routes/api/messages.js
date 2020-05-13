a7db73e724f4bdd61f3fa3cbfa08a5f84fcae4e5
Fatma Akram <fatma.khater96@gmail.com>
2020-05-14 01:33:57 +0200

    Add message routes

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -1,8 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+const Message = require('../../models/Message');
+const User = require('../../models/User');
 
-router.get('/',(req,res)=>{
-    res.send("messages route");
+// Create Message
+// POST api/messages
+router.post('/',[auth,[
+    check('text','Text is required').not().isEmpty()
+]],
+async (req,res) =>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({ errors : errors.array() })
+    } 
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const newMessage = new Message({
+            text : req.body.text,
+            user : req.user.id
+        });
+        const message = await newMessage.save();
+        res.json(message);
+    } catch (error) {
+        console.log(error.message);
+        return res.status(500).send('Server Error');
+    }
 });
+// Get all Messages
+// GET api/messages
+router.get('/', auth, async (req, res) => {
+    try {
+      const messages = await Message.find().sort({ date: -1 });
+      res.json(messages);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+// Get message by ID
+// GET api/messages/:id
 
+router.get('/:id', auth, async (req, res) => {
+    try {
+
+      const message = await Message.findById(req.params.id);
+      res.json(message);  
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+  });
+
+// Delete message
+// DELETE api/messages/:id
+router.delete('/:id', auth, async (req, res) => {
+    try {
+      const message = await Message.findById(req.params.id);
+  
+      if (message.user.toString() !== req.user.id) {
+        return res.status(401).json({ msg: 'User not authorized' });
+      }
+  
+      await message.remove();
+  
+      res.json({ msg: 'Message removed' });
+    } catch (err) {
+      console.error(err.message);
+  
+      res.status(500).send('Server Error');
+    }
+  });
+
+  
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -5,9 +5,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require("config");
 const User = require("../../models/User")
-// @route    POST api/users
-// @desc     Register user
-// @access   Public
+
 router.post(
     '/',
     [
